399561ae19d3692daa06f5a45ea3b3229d51fa03
andersongrs88 <anderson.grs88@gmail.com>
2026-01-01 13:53:49 -0300

    Delete src directory

--- src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="mt-10 border-t border-zinc-200 py-6 text-center text-sm text-zinc-600 dark:border-zinc-800 dark:text-zinc-400">
-      Desenvolvido pela Wizze Tecnologia Inteligente.
-    </footer>
-  );
-}
