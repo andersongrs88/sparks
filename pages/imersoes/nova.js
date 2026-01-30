1bd5d18598970288cdc3a24357decee1b59269d5
andersongrs88 <anderson.grs88@gmail.com>
2026-01-30 17:44:51 -0300

    Add files via upload

--- pages/imersoes/nova.js
+++ pages/imersoes/nova.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 import ImmersionTabs from "../../components/ImmersionTabs";
 import { useAuth } from "../../context/AuthContext";
-import { createImmersion } from "../../lib/immersions";
+import { createImmersion, listImmersionCatalog } from "../../lib/immersions";
 import { listActiveProfiles } from "../../lib/profiles";
 import { listTemplates } from "../../lib/templates";
 import { listSpeakers } from "../../lib/speakers";
